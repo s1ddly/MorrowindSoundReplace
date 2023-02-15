9df8107ba01c33cb05a7e5f4fce949d9aec85d94
Sid Shardanand <51441497+s1ddly@users.noreply.github.com>
2023-02-15 21:35:10 +1100

    Initial commit

---
+++ Replacer.ps1
@@ -0,0 +1,26 @@
+﻿Function Get-Folder($initialDirectory="")
+
+{
+    [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms")|Out-Null
+
+    $foldername = New-Object System.Windows.Forms.FolderBrowserDialog
+    $foldername.Description = "Select a folder"
+    $foldername.rootfolder = "MyComputer"
+    $foldername.SelectedPath = $initialDirectory
+
+    if($foldername.ShowDialog((New-Object System.Windows.Forms.Form -Property @{TopMost = $true })) -eq "OK")
+    {
+        $folder += $foldername.SelectedPath
+    }
+    return $folder
+}
+
+$mwfolder = Get-Folder
+
+echo "$mwfolder\Data Files\Sound"
+
+If(Test-Path -Path $mwfolder\Morrowind.exe -PathType Leaf){
+    echo "yeet"
+} Else {
+    echo "This isn't a Valid Morrowind folder! Exiting now."
+}
