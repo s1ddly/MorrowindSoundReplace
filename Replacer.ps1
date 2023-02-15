0f8f03d093b89f26f888f539e90efb7432ddc1e6
Sid Shardanand <51441497+s1ddly@users.noreply.github.com>
2023-02-15 22:43:47 +1100

    Adding in input file chooser

--- Replacer.ps1
+++ Replacer.ps1
@@ -15,12 +15,37 @@
     return $folder
 }
 
+Function Get-File($initialDirectory="")
+
+{
+    [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms")|Out-Null
+
+    $filename = New-Object System.Windows.Forms.OpenFileDialog
+
+    if($filename.ShowDialog((New-Object System.Windows.Forms.Form -Property @{TopMost = $true })) -eq "OK")
+    {
+        $file += $filename.FileName
+    }
+    return $file
+}
+
 $mwfolder = Get-Folder
 
 echo "$mwfolder\Data Files\Sound"
 
 If(Test-Path -Path $mwfolder\Morrowind.exe -PathType Leaf){
-    echo "yeet"
+    #Select input mp3
+    $inmp3 = Get-File
+    echo $inmp3
+    #Select input wav
+
+    #Backup?
+
+    #Copy/OverWrite
+    echo "Generating list of files to overwrite"
+    $mp3list = Get-ChildItem -Path "$mwfolder\Data Files\Sound" -Filter *.mp3 -Recurse
+    $wavlist = Get-ChildItem -Path "$mwfolder\Data Files\Sound" -Filter *.wav -Recurse
+    echo "OverWriting mp3's"
 } Else {
-    echo "This isn't a Valid Morrowind folder! Exiting now."
+    [System.Windows.MessageBox]::Show("This isn't a Valid Morrowind folder! Exiting now.", "Error", 0, "Error")
 }
