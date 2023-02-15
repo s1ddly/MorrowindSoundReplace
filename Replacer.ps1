de2cbb1c9eed59b5b421b976b5723e885ff040ff
Sid Shardanand <51441497+s1ddly@users.noreply.github.com>
2023-02-15 23:33:31 +1100

    Adding in checks for input files

--- Replacer.ps1
+++ Replacer.ps1
@@ -21,6 +21,7 @@ Function Get-File($initialDirectory="")
     [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms")|Out-Null
 
     $filename = New-Object System.Windows.Forms.OpenFileDialog
+    $filename.Title = "Choose File"
 
     if($filename.ShowDialog((New-Object System.Windows.Forms.Form -Property @{TopMost = $true })) -eq "OK")
     {
@@ -34,18 +35,34 @@ $mwfolder = Get-Folder
 echo "$mwfolder\Data Files\Sound"
 
 If(Test-Path -Path $mwfolder\Morrowind.exe -PathType Leaf){
+    
     #Select input mp3
-    $inmp3 = Get-File
+    echo "Please Choose an mp3 file"
+    $inmp3 = Get-File("","Choose .mp3 File")
     echo $inmp3
+    If(!($inmp3 -contains ".mp3")){
+        [System.Windows.MessageBox]::Show("Not a valid mp3 file! Exiting now.", "Error", 0, "Error")
+        Exit 2
+    }
+
     #Select input wav
+    echo "Please Choose a wav file"
+    $inwav = Get-File("","Choose .wav File")
+    If(!($inwav -contains ".wav")){
+        [System.Windows.MessageBox]::Show("Not a valid mp3 file! Exiting now.", "Error", 0, "Error")
+        Exit 2
+    }
 
     #Backup?
+    [System.Windows.MessageBox]::Show("Do you want to take a backup of the original sounds?", "Backup", 4, "Question")
 
     #Copy/OverWrite
     echo "Generating list of files to overwrite"
     $mp3list = Get-ChildItem -Path "$mwfolder\Data Files\Sound" -Filter *.mp3 -Recurse
     $wavlist = Get-ChildItem -Path "$mwfolder\Data Files\Sound" -Filter *.wav -Recurse
     echo "OverWriting mp3's"
+
 } Else {
     [System.Windows.MessageBox]::Show("This isn't a Valid Morrowind folder! Exiting now.", "Error", 0, "Error")
+    Exit 1
 }
